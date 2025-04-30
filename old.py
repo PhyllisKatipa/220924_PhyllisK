9cb297b0397493f81432bded874efa581c7cebd5
PhyllisKatipa <katp23171@wainuiomatahigh.school.nz>
2025-05-01 10:21:15 +1200

    Create old.py
    16/4/25

---
+++ old.py
@@ -0,0 +1,12 @@
+#Welcome the user
+print("Welcome to my Six60 quiz! ")
+
+#Chances
+chances = 1
+print("You will have", chances, "chance to answer correctly.\nPlease put the alphabet of the answer\n")
+
+#Score
+score = 0
+
+#question 1
+question_1 = print("1) What year was Six60 formed?\n(a) 2008\n(b) 2006\n(c)
